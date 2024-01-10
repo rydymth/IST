8d4ef4a3dd9cb651aa353e0797db6e928195ad18
rydymth <rudra.pednekar@gmail.com>
2024-01-10 13:49:52 +0530

    LINKS FOR ALL THE MSCIT SHIT WILL BE STORED HERE. IF NEW ONES REQUIRED THEN MSG ME

--- Assignment 7/city.sh
+++
@@ -1,5 +0,0 @@
-city="vadodara pune mumbai"
-for a in $city
-do
-	echo "city name is:$a"
-done
--- Assignment 7/even.sh
+++
@@ -1,8 +0,0 @@
-
-for (( i = 1; i <=20; i++ ))
-do
-   if [[ $i%2 -eq 0 ]]
-   then
-        echo $i
-   fi
-done
